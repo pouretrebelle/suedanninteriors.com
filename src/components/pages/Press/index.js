fb4964da74bd89857aa8e89fe26b1858c5f5f5bf
Charlotte Dann <ask@pouretrebelle.com>
2018-07-04 21:38:18 +0100

    :sparkles: Add Press page

---
+++ src/components/pages/Press/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import PageWrapper from '../../PageWrapper';
+
+import styles from './Press.module.sass';
+
+const PressPage = () => (
+  <PageWrapper title={'Press'}>
+    <ul className={styles.pressList}>
+      <li className={styles.pressItem}>
+        <h3 className={styles.pressItemTitle}>
+          <a href={'/assets/selfbuilddesignarticle.pdf'} target={'_blank'}>
+            Enhancing The Flow
+          </a>
+        </h3>
+        <p className={styles.pressItemOrigin}>
+          SelfBuild &amp; Design Magazine
+        </p>
+        <time className={styles.pressItemTime}>June 2016</time>
+      </li>
+
+      <li className={styles.pressItem}>
+        <h3 className={styles.pressItemTitle}>
+          <a
+            href={
+              'http://www.telegraph.co.uk/finance/property/advice/10520933/Best-20-interior-designers-in-Britain.html'
+            }
+            target={'_blank'}
+          >
+            Best 20 Interior Designers in Britain
+          </a>
+        </h3>
+        <p className={styles.pressItemOrigin}>The Telegraph</p>
+        <time className={styles.pressItemTime}>December 2013</time>
+      </li>
+    </ul>
+
+    <ul className={styles.linkList}>
+      <li className={styles.linkItem}>
+        <a href="http://biid.org.uk/member/sue-dann" className={styles.link}>
+          Sue Dann on the British Institute of Interior Design
+        </a>
+      </li>
+      <li className={styles.linkItem}>
+        <a
+          href="http://www.houzz.co.uk/pro/suedann/sue-dann-interiors"
+          className={styles.link}
+        >
+          Sue Dann on Houzz
+        </a>
+      </li>
+    </ul>
+  </PageWrapper>
+);
+
+export default PressPage;
---
+++ src/pages/press.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Press from '../components/pages/Press';
+
+const PressPage = () => {
+  return <Press />;
+};
+
+export default PressPage;

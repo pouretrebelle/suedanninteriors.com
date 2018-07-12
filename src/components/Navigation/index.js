b7edc73e572826d38dfb45cd465c3d598e3dca36
Charlotte Dann <ask@pouretrebelle.com>
2018-07-12 22:01:47 +0100

    Style active links

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -14,34 +14,88 @@ class Navigation extends Component {
         <ul className={styles.navList}>
           <h2 className={styles.navTitle}>Services</h2>
           <li className={styles.navItem}>
-            <Link to="/domestic" className={styles.navLink}>Domestic</Link>
+            <Link
+              to="/domestic"
+              className={styles.navLink}
+              activeClassName={styles.navLinkActive}
+            >
+              Domestic
+            </Link>
           </li>
           <li className={styles.navItem}>
-            <Link to="/kitchens" className={styles.navLink}>Bespoke Kitchens</Link>
+            <Link
+              to="/kitchens"
+              className={styles.navLink}
+              activeClassName={styles.navLinkActive}
+            >
+              Bespoke Kitchens
+            </Link>
           </li>
           <li className={styles.navItem}>
-            <Link to="/commercial" className={styles.navLink}>Commercial</Link>
+            <Link
+              to="/commercial"
+              className={styles.navLink}
+              activeClassName={styles.navLinkActive}
+            >
+              Commercial
+            </Link>
           </li>
           <li className={styles.navItem}>
-            <Link to="/products" className={styles.navLink}>Products</Link>
+            <Link
+              to="/products"
+              className={styles.navLink}
+              activeClassName={styles.navLinkActive}
+            >
+              Products
+            </Link>
           </li>
         </ul>
         <ul className={styles.navList}>
           <h2 className={styles.navTitle}>Company</h2>
           <li className={styles.navItem}>
-            <Link to="/ethos" className={styles.navLink}>Company Ethos</Link>
+            <Link
+              to="/ethos"
+              className={styles.navLink}
+              activeClassName={styles.navLinkActive}
+            >
+              Company Ethos
+            </Link>
           </li>
           <li className={styles.navItem}>
-            <Link to="/sue-dann-profile" className={styles.navLink}>Sue Dann Profile</Link>
+            <Link
+              to="/sue-dann-profile"
+              className={styles.navLink}
+              activeClassName={styles.navLinkActive}
+            >
+              Sue Dann Profile
+            </Link>
           </li>
           <li className={styles.navItem}>
-            <Link to="/testimonials" className={styles.navLink}>Testimonials</Link>
+            <Link
+              to="/testimonials"
+              className={styles.navLink}
+              activeClassName={styles.navLinkActive}
+            >
+              Testimonials
+            </Link>
           </li>
           <li className={styles.navItem}>
-            <Link to="/press" className={styles.navLink}>Press</Link>
+            <Link
+              to="/press"
+              className={styles.navLink}
+              activeClassName={styles.navLinkActive}
+            >
+              Press
+            </Link>
           </li>
           <li className={styles.navItem}>
-            <Link to="/contact" className={styles.navLink}>Contact</Link>
+            <Link
+              to="/contact"
+              className={styles.navLink}
+              activeClassName={styles.navLinkActive}
+            >
+              Contact
+            </Link>
           </li>
         </ul>
 

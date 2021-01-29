57d1fe09e441708d862aeb5c4b52a2ede57a5edd
Charlotte Dann <hi@cha.rs>
2021-01-29 20:12:17 +0000

    Updates

---
+++ src/components/PageFooter/index.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+import styles from './PageFooter.module.sass';
+
+class PageFooter extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className={styles.footer}>
+        <p>
+          Sue&nbsp;Dann&nbsp;Interiors, Studio&nbsp;S14, Old&nbsp;Bakery&nbsp;Studios, Blewetts&nbsp;Wharf, Malpas&nbsp;Road, Truro, Cornwall, TR1&nbsp;1QH. United Kingdom
+        </p>
+        <p>
+          Cornwall: +44 (0) 187 285 7249 | Mob: +44 (0) 7836 358 635
+        </p>
+        <p>
+          E:{' '}
+          <a href={'mailto:sue@suedanninteriors.com'}>
+            sue@suedanninteriors.com
+          </a>
+        </p>
+
+        <div className={styles.biidWrapper}>
+          <a href="https://biid.org.uk/users/sue-dann">
+            <img
+              src={require('../../assets/biid.png')}
+              alt="Sue is a member of the British Institute of Interior Design"
+              className={styles.biidLogo}
+            />
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PageFooter;
